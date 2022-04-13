879d29d9a49679dff124149d966fa21416cb56c8
seoh2k <seoh2k@gmail.com>
2022-04-13 13:02:40 +0900

    영화 목록(MovieList) 구현 완료 컬럼 추가, 테이블 태그 추가

--- egov-test/src/main/webapp/js/movieList.js
+++ egov-test/src/main/webapp/js/movieList.js
@@ -3,9 +3,32 @@ import movieData from './movieData.js';
 export default{
 	template : `
 		<div>
-			<div v-for="item in movieList">
-				 <div v-text="loadTxt(item.title)"></div>
-			</div>
+			<table>
+				<thead>
+					<tr>
+						<th>Number</th>
+						<th>Poster</th>
+						<th>Title</th>
+						<th>Year</th>
+						<th>Rating</th>
+						<th>Genres</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="item in movieList">
+						 <td v-text="loadTxt(item.id, 'id')"></td>
+						 <td><img v-bind:src="loadTxt(item.medium_cover_image, 'poster')"></td>
+						 <td v-text="loadTxt(item.title, 'title')"></td>
+						 <td v-text="loadTxt(item.year, 'year')"></td>
+						 <td v-text="loadTxt(item.rating, 'rating')"></td>
+						 <td>
+							<ul v-for="genres in loadTxt(item.genres, 'genres')">
+								<li v-text="genres"></li>
+							</ul>
+						 </td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	`,
 	data() {
@@ -17,8 +40,12 @@ export default{
 		this.movieList = movieData.movies;
     },
 	methods : {
-		loadTxt(data) {
-			return data;
+		loadTxt(data, type) {
+			if(type == 'title' && data.length > 30){
+				return data.slice(0,30) + '...';
+			} else {
+				return data;
+			}
 		}
 	}
 }
