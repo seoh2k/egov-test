049b6a64dc1bca0f0e53fb808ca7b57add6c542b
seoh2k <seoh2k@gmail.com>
2022-05-08 23:25:25 +0900

    [Router] 영화 입력 페이지로 이동하기 위한 라우터 구현 중 1. npm i vue-router: 라우터 설치 2. main.js에 라우터 생성 3. 라우팅 대상 페이지에 name, router-link 추가

---
+++ egov-test/src/main/webapp/js/movieList.js
@@ -0,0 +1,56 @@
+import movieData from './movieData.js';
+
+export default{
+	name: 'MovieList',
+	template : `
+		<div>
+			<button><router-link to="/MovieCreate">입력</router-link></button>
+			<table>
+				<thead>
+					<tr>
+						<th>Number</th>
+						<th>Poster</th>
+						<th>Title</th>
+						<th>Year</th>
+						<th>Rating</th>
+						<th>Genres</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="item in movieList">
+						 <td v-text="loadTxt(item.id, 'id')"></td>
+						 <td><img v-bind:src="loadTxt(item.medium_cover_image, 'poster')"></td>
+						 <td v-text="loadTxt(item.title, 'title')"></td>
+						 <td v-text="loadTxt(item.year, 'year')"></td>
+						 <td v-text="loadTxt(item.rating, 'rating')"></td>
+						 <td>
+							<ul v-for="genres in loadTxt(item.genres, 'genres')">
+								<li v-text="genres"></li>
+							</ul>
+						 </td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	`,
+	data() {
+		return {
+			movieList : {}
+		};
+	},
+	mounted() {
+		this.movieList = movieData.movies;
+	},
+	methods : {
+		loadTxt(data, type) {
+			if(type == 'title' && data.length > 30){
+				return data.slice(0,30) + '...';
+			} else {
+				return data;
+			}
+		},
+		create() {
+			this.$router.push('MovieCreate')
+		}
+	}
+}
