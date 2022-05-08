acef4ef5e59eb8511b8ae0293289f0c62760eb72
seoh2k <seoh2k@gmail.com>
2022-05-08 23:35:06 +0900

    [Git] 커밋 충돌 해결 - 커밋 전에는 꼭 pull 받기

--- egov-test/src/main/webapp/js/movieList.js
+++ egov-test/src/main/webapp/js/movieList.js
@@ -1,7 +1,6 @@
 import movieData from './movieData.js';
 
 export default{
-<<<<<<< HEAD
 	name: 'MovieList',
 	template : `
 		<div>
@@ -53,54 +52,5 @@ export default{
 		create() {
 			this.$router.push('MovieCreate')
 		}
-		
-=======
-	template : `
-		<div>
-			<table>
-				<thead>
-					<tr>
-						<th>Number</th>
-						<th>Poster</th>
-						<th>Title</th>
-						<th>Year</th>
-						<th>Rating</th>
-						<th>Genres</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="item in movieList">
-						 <td v-text="loadTxt(item.id, 'id')"></td>
-						 <td><img v-bind:src="loadTxt(item.medium_cover_image, 'poster')"></td>
-						 <td v-text="loadTxt(item.title, 'title')"></td>
-						 <td v-text="loadTxt(item.year, 'year')"></td>
-						 <td v-text="loadTxt(item.rating, 'rating')"></td>
-						 <td>
-							<ul v-for="genres in loadTxt(item.genres, 'genres')">
-								<li v-text="genres"></li>
-							</ul>
-						 </td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	`,
-	data() {
-		return {
-			movieList : {}
-		};
-	},
-	mounted() {
-		this.movieList = movieData.movies;
-    },
-	methods : {
-		loadTxt(data, type) {
-			if(type == 'title' && data.length > 30){
-				return data.slice(0,30) + '...';
-			} else {
-				return data;
-			}
-		}
->>>>>>> branch 'master' of https://github.com/seoh2k/egov-test.git
 	}
 }
